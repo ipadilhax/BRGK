506e35e2b36254cd4a93405afc8d8c61f6d4343e
Ruan Padilha <ruanpadilha345@gmail.com>
2023-05-30 13:58:16 -0300

    commit

---
+++ js/signin.js
@@ -0,0 +1,23 @@
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+const appendAlert = (message, type) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('')
+
+  alertPlaceholder.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('liveAlertBtn')
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', () => {
+    appendAlert('Inscrito com sucesso!', 'success')
+    sucesso = document.querySelector(".formContact")
+    sucesso.style.display = "none"
+    sucesso2 = document.querySelector(".sucesso")
+    sucesso2.style.display = "block"
+  })
+}
